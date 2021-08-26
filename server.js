38255203fd8830db0cca418e51013a1beab58590
AshisPatel <ashis.n.patel@gmail.com>
2021-08-26 16:24:40 -0500

    add POST function to accept user data and add it

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-const express = require('express'); 
-const { animals } = require('./data/animals')
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const { animals } = require('./data/animals');
+const { send } = require('process');
 
 // Instantiate the server
 
-const app = express(); 
+const app = express();
 
-const PORT = process.env.PORT || 3001; 
+// parse incoming string or array data
+
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
 
 function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = []; 
+    let personalityTraitsArray = [];
 
-    let filteredResults = animalsArray; 
+    let filteredResults = animalsArray;
 
-    if(query.personalityTraits) {
+    if (query.personalityTraits) {
         if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits]; 
+            personalityTraitsArray = [query.personalityTraits];
         } else {
             personalityTraitsArray = query.personalityTraits;
         }
@@ -33,34 +45,74 @@ function filterByQuery(query, animalsArray) {
     }
 
     if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name); 
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
 
-    return filteredResults; 
+    return filteredResults;
 }
 
 function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0]; 
-    return result; 
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
 }
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
-        results = filterByQuery(req.query, results); 
+        results = filterByQuery(req.query, results);
     }
-    res.json(results); 
+    res.json(results);
 });
 
 app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals); 
+    const result = findById(req.params.id, animals);
     if (result) {
         res.json(result);
     } else {
-        res.send(404); 
+        res.send(404);
     }
-}); 
+});
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where the incoming content will be stored
+    req.body.id = animals.length.toString();
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted');
+    } else {
+        const animal = createNewAnimal(req.body, animals)
+
+        res.json(animal);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-}); 
+});
